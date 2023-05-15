59b71c18a3483e773cccca4f7831f2aae2775ded
jolive.hodehou <49763191+hodehoujolive@users.noreply.github.com>
2023-05-15 17:39:06 +0100

    added a read_config_section function to read sections in the configuration file

--- utils/config_reader.py
+++ utils/config_reader.py
@@ -4,3 +4,8 @@ def read_config(section, key):
     config = configparser.ConfigParser()
     config.read('config/test_config.ini')
     return config[section][key]
+
+def read_config_section(section):
+    config = configparser.ConfigParser()
+    config.read('config/test_config.ini')
+    return config[section]
